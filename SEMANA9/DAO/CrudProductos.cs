0d7122b5b6b390c29c194f4e5866256a67dfdd1b
Henriquez77 <jhonatanernesto.henrriquez@gmail.com>
2023-03-21 20:02:09 -0600

    Tarea semana 9

---
+++ SEMANA9/DAO/CrudProductos.cs
@@ -0,0 +1,53 @@
+﻿using SEMANA9.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace SEMANA9.DAO
+{
+    public class CrudProductos
+    {
+        public int Insert(Producto producto)
+        {
+            //Insert
+            using (AlmacenContext context = new AlmacenContext())
+            {
+                context.Add(producto);
+                return context.SaveChanges();
+            }
+        }
+        public Producto SelectById(Producto producto)
+        {
+            //SelectById
+            using (AlmacenContext context = new AlmacenContext())
+            {
+                return context.Productos.FirstOrDefault(x => x.Id == producto.Id);
+            }
+        }
+        public int Update(Producto producto)
+        {
+            using (AlmacenContext context = new AlmacenContext())
+            {
+                context.Update(producto);
+                return context.SaveChanges();
+            }
+        }
+        public List<Producto> SelectAll()
+        {
+            using (AlmacenContext context = new AlmacenContext())
+            {
+                return context.Productos.ToList();
+            }
+        }
+        public int Delete(Producto producto)
+        {
+            using (AlmacenContext context = new AlmacenContext())
+            {
+                context.Productos.Remove(producto);
+                return context.SaveChanges();
+            }
+        }
+    }
+}
